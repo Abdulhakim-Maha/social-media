a8afdb942ce87bbb97f97b3900c2ece38b6f15e0
austiniqer <63011075@kmitl.ac.th>
2021-07-12 02:03:28 +0700

    first start with client

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import TopBar from "./components/topbar/TopBar";
+import Home from "./pages/home/Home";
+
+function App() {
+  return <TopBar />;
+}
+
+export default App;
---
+++ client/src/components/topbar/TopBar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import classes from './TopBar.module.css'
+import SearchIcon from '@material-ui/icons/Search';
+import PersonIcon from '@material-ui/icons/Person';
+import {Chat, Notifications, SearchRounded} from '@material-ui/icons'
+
+function TopBar() {
+	return (
+		<div className={classes.topbarContainer}>
+			<div className={classes.topbarLeft}>
+				<span className={classes.logo}>Facebook</span>
+			</div>
+			<div className={classes.topbarCenter}>
+				<div className={classes.searchBar}>
+					<SearchIcon className={classes.searchIcon} />
+					<input type="text" placeholder="Search for friend, post or video"  className={classes.searchInput} />
+				</div>
+			</div>
+			<div className={classes.topbarRight}>
+				<div className={classes.topbarLinks}>
+					<span className={classes.topbarLink}>Homepage</span>
+					<span className={classes.topbarLink}>Timeline</span>
+				</div>
+				<div className={classes.topbarIcons}>
+					<div className={classes.topbarIconItem}>
+						<PersonIcon/>
+						<span className={classes.topbarIconBadge}>1</span>
+					</div>
+					<div className={classes.topbarIconItem}>
+						<Chat/>
+						<span className={classes.topbarIconBadge}>2</span>
+					</div>
+					<div className={classes.topbarIconItem}>
+						<Notifications/>
+						<span className={classes.topbarIconBadge}>1</span>
+					</div>
+				</div>
+				<img src="/assets/person/2.jpeg" alt="" className={classes.topbarImg}/>
+			</div>
+		</div>
+	)
+}
+
+export default TopBar
